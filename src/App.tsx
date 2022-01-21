038f4d6838fb659e0cef837a882c053d938f625e
André Sousa <andre.sousa@andresousa.pt>
2022-01-21 20:47:08 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@material-ui/core'
 import MailIcon from '@material-ui/icons/Mail'
 import { SocialIcon } from 'react-social-icons';
-import CountdownTimer from './components/CountdownTimer';
 import Logo from './components/Logo';
 
 const App = () => {
@@ -10,13 +9,13 @@ const App = () => {
         <div className="App">
             <div className="content">
                 <Logo />
-                <CountdownTimer text="days to the beginning of a new career!" />
+                <h1>Coming Soon...</h1>
                 <Button href="mailto:andre.sousa@andresousa.pt?subject=André Sousa - Contact" variant="contained" color="secondary" startIcon={<MailIcon />}>
                     Get in touch
                 </Button>
                 <div className="social-links">
-                    <SocialIcon url="https://www.linkedin.com/in/andr%C3%A9-sousa-324032208/" fgColor="white"/>
-                    <SocialIcon url="https://github.com/andresousa23" bgColor="white" fgColor="black"/>
+                    <SocialIcon url="https://www.linkedin.com/in/andresousadotpt" fgColor="white"/>
+                    <SocialIcon url="https://github.com/andresousadotpt" bgColor="white" fgColor="black"/>
                 </div>
             </div>
         </div>
